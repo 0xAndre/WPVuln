e1d65ec187d1047621d05a9b2663cbec8bcb4a7e
André <ancatos@outlook.com>
2019-01-04 00:08:39 +0000

    update app  readme and vulns.json

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # WPVuln
-Vulnerability Scanner for Wordpress
+
+<p align="center">
+  <img src="https://i.imgur.com/1E8oXSx.png"/>
+</p>
+
+WPVuln is a vulnerability scanner for Wordpress.
+  - Scan the most recent discovered vulnerabilities
+
+### Installation
+
+WPVuln requires [Node.js](https://nodejs.org/) to run.
+
+```sh
+$ git clone https://github.com/x0uter/WPVuln.git
+$ cd WPVuln
+$ npm install
+```
+
+### Usage
+
+```sh
+$ npm start "url"
+```
+
+### Example
+
+```sh
+$ npm start http://andresantos.pt
+```
--- app.js
+++ app.js
@@ -8,26 +8,62 @@ const B_RED = '\x1b[41m'
 const RESET = '\x1b[0m'
 const B_YELLOW = '\x1b[43m'
 const B_GREEN = '\x1b[42m'
+const P_RED = '\x1b[31m'
 
 /**
  * URL ARG
  */
 const url = process.argv[2]
 
+/**
+ * CORE
+ */
+header()
 try {
-    if(!url) throw 'Provide a url!'
-
+    if (!url) throw 'Provide a url!'
+    let vuln_count = 0
+    let total = 0
     console.log(B_GREEN, `Scanning ${url}... \n`)
 
     vulns.forEach((v) => {
         let dynUrl = `${url}/wp-content/${v.iname}/`
         request(dynUrl, function (error, response, body) {
             if (response.statusCode === 200) {
-                console.log(B_YELLOW,`[ ALERT ] Vulnerability discovered at ${dynUrl} | Plugin: ${v.name} | Vulnerability Type: ${v.vuln_type}`, RESET)
+                console.log(B_YELLOW, `[ ALERT ] Vulnerability discovered at ${dynUrl} | Plugin: ${v.name} | Vulnerability Type: ${v.vuln_type}`, RESET)
+                vuln_count++
+            }
+
+            if (++total === vulns.length) {
+                if (vuln_count === 0) {
+                    console.log(B_GREEN, `[ OK ] No Vulnerabilities found!`, RESET)
+                } else {
+                    console.log(B_RED, `[ !!! ] Discovered ${vuln_count} Vulnerabilities!`, RESET)
+                }
             }
         })
     })
 
-} catch(ex){
-    console.log(B_RED,`[ ERROR ] ${ex}`,RESET)
+} catch (ex) {
+    console.log(B_RED, `[ ERROR ] ${ex}`, RESET)
 }
+
+
+/**
+ * VISUALS
+ */
+
+ function header(){
+     console.log(P_RED,`
+     ###############################################################################################
+
+                    >=>        >=> >======>   >=>         >=>           >=>           
+                    >=>        >=> >=>    >=>  >=>       >=>            >=>           
+                    >=>   >>   >=> >=>    >=>   >=>     >=>   >=>  >=>  >=> >==>>==>  
+                    >=>  >=>   >=> >======>      >=>   >=>    >=>  >=>  >=>  >=>  >=> 
+                    >=> >> >=> >=> >=>            >=> >=>     >=>  >=>  >=>  >=>  >=> 
+                    >> >>    >===> >=>             >===>      >=>  >=>  >=>  >=>  >=> 
+                    >=>        >=> >=>              >=>         >==>=> >==> >==>  >=>
+    
+    ###############################################################################################
+    `, RESET, '\n')
+ }
